c0a8ae160883fb9c7f776b063ceecf4dd5e06067
ChristopherBlack <radsk8dude@gmail.com>
2020-03-30 18:15:09 -0700

    My first Python program

---
+++ Python for Beginners/print.py
@@ -0,0 +1,10 @@
+print('Adding numbers')
+x = 42 + 206
+print('Performing division')
+y = x / 2
+z = x % 2
+
+print('Math complete')
+print('\nx is ', x)
+print('y is ', y)
+print('z is ', z)
---
+++ Python for Beginners/strings.py
@@ -0,0 +1,10 @@
+# first_name = 'Christopher'
+# last_name = 'Black'
+
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+# print(first_name + last_name)
+print('Hello, ' + first_name.capitalize() + ' ' + last_name.capitalize())
+
+print('Hello, ' + first_name.upper())
+print('Hello, ' + last_name.lower())
