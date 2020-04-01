1f85fa0d815bfc84c39a018a8c97c5cb9ecdfe52
ChristopherBlack <radsk8dude@gmail.com>
2020-03-31 18:27:15 -0700

    new file:   Python for Beginners/havefun.py 	new file:   Python for Beginners/numbers.py 	new file:   Python for Beginners/strings2.py

---
+++ Python for Beginners/numbers.py
@@ -0,0 +1,11 @@
+# pi = 3.14159
+# print(pi)
+
+# first_num = input ('Please enter a number: ')
+# second_num = input ('Please enter another number: ')
+# # first_num = 5
+# # second_num = 6
+# print(int(first_num) ** int(second_num))
+
+days_in_Feberuary = 28 
+print(str(days_in_Feberuary) + ' total days in February')
---
+++ Python for Beginners/strings2.py
@@ -0,0 +1,13 @@
+first_name = 'Christopher'
+last_name = 'Black'
+
+output = ' Hello, ' + first_name + '' + last_name
+output = '2. Hello , {} {}'.format(first_name, last_name)
+output = '3. Hello , {0} {1}'.format(first_name, last_name)
+output = '4. Hello , {1} {0}'.format(first_name, last_name)
+output = f'5. Hello {first_name} {last_name}'
+print(output)
+print(output)
+print(output)
+print(output)
+print(output)
